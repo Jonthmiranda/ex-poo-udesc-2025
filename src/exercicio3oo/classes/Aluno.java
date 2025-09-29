39f638d78c420420fd8ea5e007f6450ef315b636
jonth_miranda <miranda.jonathan@outlook.com>
2025-09-29 15:15:45 -0300

    corrigido getters e setters

--- src/exercicio3oo/classes/Aluno.java
+++ src/exercicio3oo/classes/Aluno.java
@@ -17,15 +17,15 @@ public class Aluno {
     public int getIdade() { return idade; }
     public void setIdade(int idade) { this.idade = idade; }
 
-    public int getNota1 { return nota1; }
+    public int getNota1() { return nota1; }
     public void setNota1(int nota1) { this.nota1 = nota1; }
 
-    public int getNota2 { return nota2; }
+    public int getNota2() { return nota2; }
     public void setNota2(int nota2) { this.nota2 = nota2; }
 
-    public int getNota3 { return nota3; }
+    public int getNota3() { return nota3; }
     public void setNota3(int nota3) { this.nota3 = nota3; }
 
-    public int getNota4 { return nota4; }
+    public int getNota4() { return nota4; }
     public void setNota4(int nota4) { this.nota4 = nota4; }
 }
--- src/exercicio3oo/classes/ContaBancaria.java
+++ src/exercicio3oo/classes/ContaBancaria.java
@@ -12,5 +12,5 @@ public class ContaBancaria {
     public void setTitular(String titular) { this.titular = titular; }
 
     public double getSaldo() { return saldo; }
-    public void setSaldo(double titular) { this.titular = titular; }
+    public void setSaldo(double saldo) { this.saldo = saldo; }
 }
